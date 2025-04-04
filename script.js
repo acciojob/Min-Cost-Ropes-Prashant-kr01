f09c8e602895c474afa02ca19de517fa63de9755
Prashant kumar <114512068+Prashant-kr01@users.noreply.github.com>
2025-04-04 15:29:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function mincost(arr)
-{ 
-arr.sort((a,b)=> a-b);
-// console.log(arr);
-let totalcost =0;
-    
+{
+  //write your code here
+    arr.sort((a,b)=>a-b)
 
-while(arr.length>1){
-let rope1 = arr.shift();
-let rope2 = arr.shift();
-let cost = rope1 + rope2 ;
-totalcost += cost;
-arr.push(cost);
-arr.sort((a,b)=> a-b);
+    let cost = 0
+    //console.log(arr)
+    while(arr.length>1){
+        let first = arr.shift()
+        let second = arr.shift()
+
+        cost+=first+second
+
+        arr.push(first+second)
+        arr.sort((a,b)=>a-b)
+      //  console.log(arr)
+    }
+
+    return cost
 }
 
-return totalcost;
-  
-}
- let result = mincost();
- console.log(result);
- module.exports = mincost;
+module.exports=mincost;
